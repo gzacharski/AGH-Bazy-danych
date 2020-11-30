b31891407db5a754c8470b6deb3d6dae01f3977b
gzacharski <g.zacharski@wp.pl>
2020-11-30 23:23:19 +0100

    Add middleware

---
+++ middlewares/switchToNode.js
@@ -0,0 +1,28 @@
+module.exports.select = (request, response, next) => {
+
+    console.log(request.path);
+
+    let label = request.path.split('/')[2];
+    label=mapPathToNode(label);
+    
+    console.log(label);
+
+    request.headers['node_label']= label;
+    next();
+}
+
+const mapPathToNode = (label) => {
+
+    switch (label) {
+        case 'suppliers':
+            label='Supplier';
+            break;
+        case 'products':
+            label='Product';
+            break;
+        default:
+            console.log(`There is no such a expression.`);
+    }
+
+    return label;
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router=express.Router();
 
 const CustomerController=require('../controllers/CustomerController');
 const SupplierController=require('../controllers/SupplierController');
+const Controller=require('../controllers/Controller');
+const switchToNode=require('../middlewares/switchToNode');
 
 router.get('/api/customers', CustomerController.getAll);
 router.get('/api/customers/:id',CustomerController.getById);
@@ -10,7 +12,7 @@ router.post('/api/customers',CustomerController.create)
 router.put('/api/customers/:id',CustomerController.updateById);
 router.delete('/api/customers/:id',CustomerController.deleteById);
 
-router.get('/api/suppliers', SupplierController.getAll);
+router.get('/api/suppliers', switchToNode.select, Controller.getAll);
 router.get('/api/suppliers/:id',SupplierController.getById);
 router.post('/api/suppliers',SupplierController.create)
 router.put('/api/suppliers/:id',SupplierController.updateById);
