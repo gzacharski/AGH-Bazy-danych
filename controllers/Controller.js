223857b1b7cbbf5947e0f6332acba6313694ae16
bartoszkordek <bartosz.kordek10@gmail.com>
2020-12-12 01:24:34 +0100

    Enhanced relationships: Supplies, Ordered By New relationship: Belongs To

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -247,12 +247,23 @@ module.exports.createOrderedByRelation = async (request, response) => {
     const orderNodeLabel = 'Order';
 
     try {
-        const relationOrderQuery = `MATCH (a:${orderNodeLabel}),(b:${customerNodeLabel}) WHERE a.id = ${orderId} AND b.id = '${customerId}'CREATE (a)-[r:ORDERED_BY]->(b) RETURN type(r)`;
+        const existingOrderCustomersQuery = `MATCH (o:${orderNodeLabel})-[r:ORDERED_BY]-(c:${customerNodeLabel}) WHERE o.id = $id RETURN c`;
+        const existingOrderCustomersParams =  {id: Number.parseInt(orderId)};
+        const existingOrderCustomersResult = await session.writeTransaction(tx => tx.run(existingOrderCustomersQuery, existingOrderCustomersParams));
+        const customersCount = existingOrderCustomersResult.records.length;
 
-        const result = await session.writeTransaction(tx => tx.run(relationOrderQuery));
+        if(customersCount != 0){
+            const existingCustomer = existingOrderCustomersResult.records[0].get(0).properties.id;
+            throw new Error(`ERROR - cannot create ORDERED BY relationship between Order: ${orderId} and Customer: ${customerId} Order: ${orderId} has already got Customer ${existingCustomer}.`);
+        }
+
+        const relationOrderQuery = `MATCH (o:${orderNodeLabel}),(c:${customerNodeLabel}) WHERE o.id = $order AND c.id =$customer CREATE (o)-[r:ORDERED_BY]->(c) RETURN type(r)`;
+
+        const params =  {order: Number.parseInt(orderId), customer: customerId};
+        const result = await session.writeTransaction(tx => tx.run(relationOrderQuery, params));
         const node = result.records[0];
 
-        if (!node) throw new Error(`ERROR - cannot create ORDERED BY relationship between order: ${orderId} and product: ${customerId} `);
+        if (!node) throw new Error(`ERROR - cannot create ORDERED BY relationship between Order: ${orderId} and Customer: ${customerId} `);
 
         response
             .status(200)
@@ -275,6 +286,75 @@ module.exports.createOrderedByRelation = async (request, response) => {
 
 }
 
+module.exports.getCustomersWhoOrderedOrder = async (request, response) => {
+    console.log('Get Customers Who ordered the Order...');
+
+    const id = request.params.id;
+    const session = driver.session(config);
+
+    try{
+        const query = `MATCH (order:Order)-[r:ORDERED_BY]->(customer:Customer) WHERE order.id=$id RETURN customer`;
+        const params =  {id: Number.parseInt(id) };
+
+        const result = await session.readTransaction(tx => tx.run(query,params));
+        const nodes = result.records.map(record => record.get(0).properties);
+
+        if (!nodes) throw new Error(`The server was not able to get Customers who ordered the Order: ${id}.`);
+
+        response
+            .status(200)
+            .send({
+                quantity: nodes.length,
+                nodes
+            })
+    }
+
+    catch (error) {
+        response
+            .status(500)
+            .send({
+                error: error.message
+            });
+    } finally {
+        await session.close();
+    }
+}
+
+module.exports.getOrdersOrderedByCustomer = async (request, response) => {
+    console.log('Get Orders ordered by Customer...');
+
+    const id = request.params.id;
+    const session = driver.session(config);
+
+    try{
+        const query = `MATCH (order:Order)-[r:ORDERED_BY]->(customer:Customer) WHERE customer.id=$id RETURN order`;
+        const params =  {id: id };
+
+        const result = await session.readTransaction(tx => tx.run(query,params));
+        const nodes = result.records.map(record => record.get(0).properties);
+
+        if (!nodes) throw new Error(`The server was not able to get Order ordered by Customer: ${id}.`);
+
+        response
+            .status(200)
+            .send({
+                quantity: nodes.length,
+                nodes
+            })
+    }
+
+    catch (error) {
+        response
+            .status(500)
+            .send({
+                error: error.message
+            });
+    } finally {
+        await session.close();
+    }
+}
+
+
 //create supplies relation
 module.exports.createSuppliesRelation = async (request, response) => {
     console.log('Create supplies relation...');
@@ -286,9 +366,19 @@ module.exports.createSuppliesRelation = async (request, response) => {
     const productNodeLabel = 'Product';
 
     try {
-        const relationSuppliesQuery = `MATCH (a:${supplierNodeLabel}),(b:${productNodeLabel}) WHERE a.id = ${supplierId} AND b.id = ${productId} CREATE (a)-[r:SUPPLIES]->(b) RETURN type(r)`;
+        const existingProductSuppliersQuery = `MATCH (s:${supplierNodeLabel})-[r:SUPPLIES]-(p:${productNodeLabel}) WHERE p.id = $id RETURN s`;
+        const existingProductSuppliersParams =  {id: Number.parseInt(productId)};
+        const existingProductSuppliersResult = await session.writeTransaction(tx => tx.run(existingProductSuppliersQuery, existingProductSuppliersParams));
+        const suppliersCount = existingProductSuppliersResult.records.length;
+        if(suppliersCount != 0) {
+            const existingSupplier = existingProductSuppliersResult.records[0].get(0).properties.id;
+            throw new Error(`ERROR - cannot create SUPPLIES relationship between supplier: ${supplierId} and product: ${productId}. Product: ${productId} has already got Supplier ${existingSupplier}.`);
+        }
 
-        const result = await session.writeTransaction(tx => tx.run(relationSuppliesQuery));
+        const relationSuppliesQuery = `MATCH (s:${supplierNodeLabel}),(p:${productNodeLabel}) WHERE s.id = $supplier AND p.id = $product CREATE (s)-[r:SUPPLIES]->(p) RETURN type(r)`;
+
+        const params =  {supplier: Number.parseInt(supplierId), product:  Number.parseInt(productId)};
+        const result = await session.writeTransaction(tx => tx.run(relationSuppliesQuery,params));
         const node = result.records[0];
 
         if (!node) throw new Error(`ERROR - cannot create SUPPLIES relationship between supplier: ${supplierId} and product: ${productId} `);
@@ -362,6 +452,122 @@ module.exports.getSuppliersWhichSupplyProduct = async (request, response) => {
         const nodes = result.records.map(record => record.get(0).properties);
 
         if (!nodes) throw new Error(`The server was not able to get Suppliers which supply Product: ${id}.`);
+
+        response
+            .status(200)
+            .send({
+                quantity: nodes.length,
+                nodes
+            })
+    }
+
+    catch (error) {
+        response
+            .status(500)
+            .send({
+                error: error.message
+            });
+    } finally {
+        await session.close();
+    }
+}
+
+//create belongs to relation
+module.exports.createBelongsToRelation = async (request, response) => {
+    console.log('Create belongs to relation...');
+
+    const productId = request.params.product;
+    const categoryId = request.params.category;
+    const session = driver.session(config);
+    const productNodeLabel = 'Product';
+    const categoryNodeLabel = 'Category';
+
+    try {
+        const existingProductCategoriesQuery = `MATCH (p:${productNodeLabel})-[r:BELONGS_TO]-(c:${categoryNodeLabel}) WHERE p.id = $id RETURN c`;
+        const existingProductCategoriesParams =  {id: Number.parseInt(productId)};
+        const existingProductCategoriesResult = await session.writeTransaction(tx => tx.run(existingProductCategoriesQuery, existingProductCategoriesParams));
+        const categoriesCount = existingProductCategoriesResult.records.length;
+        if(categoriesCount  != 0) {
+            const existingCategory = existingProductCategoriesResult .records[0].get(0).properties.id;
+            throw new Error(`ERROR - cannot create BELONGS TO relationship between Product: ${productId} and Category: ${categoryId}. Product: ${productId} has already got Category ${existingCategory}.`);
+        }
+
+        const belongsToSuppliesQuery = `MATCH (p:${productNodeLabel}),(c:${categoryNodeLabel}) WHERE p.id = $product AND c.id = $category CREATE (p)-[r:BELONGS_TO]->(c) RETURN type(r)`;
+
+        const params =  {product: Number.parseInt(productId), category: Number.parseInt(categoryId)};
+        const result = await session.writeTransaction(tx => tx.run(belongsToSuppliesQuery,params));
+        const node = result.records[0];
+
+        if (!node) throw new Error(`ERROR - cannot create BELONGS TO relationship between Product: ${productId} and Category: ${categoryId}.`);
+
+        response
+            .status(200)
+            .send({
+                quantity: node.length,
+                node
+            })
+
+    } catch (error) {
+        //send response with status 500 if error took place
+        response
+            .status(500)
+            .send({
+                message: error.message
+            });
+
+    } finally {
+        await session.close();
+    }
+}
+
+module.exports.getProductCategories = async (request, response) => {
+    console.log('Get Product Categories...');
+
+    const id = request.params.id;
+    const session = driver.session(config);
+
+    try{
+        const query = `MATCH (product:Product)-[r:BELONGS_TO]->(category:Category) WHERE product.id=$id RETURN category`;
+        const params =  {id: Number.parseInt(id) };
+
+        const result = await session.readTransaction(tx => tx.run(query,params));
+        const nodes = result.records.map(record => record.get(0).properties);
+
+        if (!nodes) throw new Error(`The server was not able to get Categories for which belongs Product: ${id}.`);
+
+        response
+            .status(200)
+            .send({
+                quantity: nodes.length,
+                nodes
+            })
+    }
+
+    catch (error) {
+        response
+            .status(500)
+            .send({
+                error: error.message
+            });
+    } finally {
+        await session.close();
+    }
+}
+
+module.exports.getProductsBelongToCategory = async (request, response) => {
+    console.log('Get Products which belong to Category...');
+
+    const id = request.params.id;
+    const session = driver.session(config);
+
+    try{
+        const query = `MATCH (product:Product)-[r:BELONGS_TO]->(category:Category) WHERE category.id=$id RETURN product`;
+        const params =  {id: Number.parseInt(id) };
+
+        const result = await session.readTransaction(tx => tx.run(query,params));
+        const nodes = result.records.map(record => record.get(0).properties);
+
+        if (!nodes) throw new Error(`The server was not able to get Products which belong to Product: ${id}.`);
 
         response
             .status(200)
