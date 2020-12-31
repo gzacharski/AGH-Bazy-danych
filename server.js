b4bf72455e5fa53057d7f8e7149310dfbb32a74b
g.zacharski <g.zacharski@wp.pl>
2020-12-31 14:20:31 +0100

    Front-end and back-end ports set

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app=require('./app');
+const serverPort=require('./config/appconfig');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || serverPort);
 
 const server=app.listen(app.get('port'), ()=>{
     console.log(`Listening on ${server.address().port}`);
