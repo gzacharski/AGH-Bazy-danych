28ed87aefca382f294f277e63d7498e37ff9e2fa
g.zacharski <g.zacharski@wp.pl>
2021-01-04 10:44:22 +0100

    Enable pagination in table

--- front-end/src/components/Table.js
+++ front-end/src/components/Table.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useTable } from 'react-table';
+import { useTable, usePagination } from 'react-table';
+import { GrPrevious, GrNext } from 'react-icons/gr'
 import './Table.css';
 
 export default function Table(props) {
@@ -8,42 +9,83 @@ export default function Table(props) {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        rows,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
+        state,
         prepareRow
     } = useTable({
         columns: props.columns,
         data: props.data
-    })
+    }, usePagination)
+
+    const {pageIndex, pageSize}=state;
 
     return (
-        <table {...getTableProps()} className="table table-striped table-hover">
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {
-                            headerGroup.headers.map(colum => (
-                                <th {...colum.getHeaderProps()} className="text-center align-middle">{colum.render('Header')}</th>
-                            ))
-                        }
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td {...cell.getCellProps()}>
-                                        <span className="cell">{cell.render('Cell')}</span>
-                                    </td>
-                                )
-                            })}
+        <>
+            <div className="d-flex justify-content-between">
+                <button
+                    onClick={() => previousPage()}
+                    disabled={!canPreviousPage}
+                    className="btn btn-light"
+                >
+                    <GrPrevious />
+                </button>
+                <button
+                    onClick={() => nextPage()}
+                    disabled={!canNextPage}
+                    className="btn btn-light"
+                >
+                    <GrNext />
+                </button>
+            </div>
+            <table {...getTableProps()} className="table table-striped table-hover">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(colum => (
+                                    <th {...colum.getHeaderProps()} className="text-center align-middle">
+                                        {colum.render('Header')}
+                                    </th>
+                                ))
+                            }
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            <span className="cell">
+                                                {cell.render('Cell')}
+                                            </span>
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <div className="d-flex justify-content-center">
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>
+                </span>
+            </div>
+        </>
     );
 }
