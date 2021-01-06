a0478406840dbae91895738f43c2651cd89a51cf
bartoszkordek <bartosz.kordek10@gmail.com>
2021-01-06 21:44:43 +0100

    Customers served by Supplier

--- test/productsTest.js
+++ test/productsTest.js
@@ -539,3 +539,19 @@ describe("Create Product Order", () => {
             });
     });
 });
+
+describe("Get Customer served by Supplier", () => {
+    const supplierId = 1;
+    const fromDate = '1999-12-01';
+    const toDate = '2001-11-01';
+    it("Should Get Customer served by Supplier when valid request", done => {
+        chai
+            .request(app)
+            .get("/api/suppliers/"+supplierId+"/customers/"+fromDate+"/"+toDate+"/onequery")
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.text).contains("unique")
+                done();
+            });
+    });
+});
