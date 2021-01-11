8a9dd1dd1ec57f92d221c82c1e9a43513f6159b4
g.zacharski <g.zacharski@wp.pl>
2021-01-11 21:29:03 +0100

    Installed package cors. Change cors function.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 const routes=require('./routes/index');
 
@@ -10,11 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use('*',cors());
 
 app.use('/',routes);
 
