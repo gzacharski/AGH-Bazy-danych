19e1985f8e0db48882210a1e8325b63eec480745
g.zacharski <g.zacharski@wp.pl>
2021-01-23 00:44:23 +0100

    Docker test on windows

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,6 @@ const serverPort=require('./config/appconfig');
 
 app.set('port', process.env.PORT || serverPort);
 
-const server=app.listen(app.get('port'), '0.0.0.0', ()=>{
+const server=app.listen(app.get('port'), ()=>{
     console.log(`Listening on ${server.address().port}`);
 });
